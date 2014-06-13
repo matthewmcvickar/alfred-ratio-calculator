41fedd2aa32032643ae190b0fe83f081b4c49b3a
Matthew McVickar <matthew@matthewmcvickar.com>
2014-06-12 19:05:03 -0700

    Fixed syntax errors.

--- workflow/calculate-ratio.rb
+++ workflow/calculate-ratio.rb
@@ -14,15 +14,19 @@ class RatioCalculator
   protected
 
   def placeholder?(number)
+    !number?(number)
+  end
+
+  def number?(number)
     /[0-9]+/.match(number.to_s)
   end
 
   def ratio_for_index(index)
     case index
-      when 0 then ratio_via_multiplication(b, c, d)
-      when 1 then ratio_via_division(a, c, d)
-      when 2 then ratio_via_multiplication(d, a, b)
-      when 3 then ratio_via_division(c, a, b)
+    when 0 then ratio_via_multiplication(b, c, d)
+    when 1 then ratio_via_division(a, c, d)
+    when 2 then ratio_via_multiplication(d, a, b)
+    when 3 then ratio_via_division(c, a, b)
     end
   end
 
